b2b8d65fdce8828b91c779454ab287516b7f76d7
alexs <alejandro.soriano@estudiantat.upc.edu>
2023-01-26 12:30:07 +0100

    Migrated from Create React App (CRA) to Vite

---
+++ src/Components/Matrix.jsx
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react';
+import '../themes/Matrix.css'
+import Node from './Node.jsx'
+export default function Matrix({matrix, setMatrix, setEndNode}) {
+
+    return (
+        <div className="grid" >
+          {matrix.map((row, rowIndex) =>
+            row.map((cell, cellIndex) => (
+                <Node rowIndex={rowIndex+1} cellIndex={cellIndex+1} value={cell} style={{gridRow: `${rowIndex+1}`, gridColumn: `${cellIndex+1}`}} key={`${rowIndex}-${cellIndex}`  } matrix={matrix} setMatrix={setMatrix} setEndNode={setEndNode}>
+                    </Node>
+            )) 
+          )}
+        </div>
+      );
+}
